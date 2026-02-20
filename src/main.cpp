0c2a180b3e06c5c434b2bb5f6aae8347f7ea1397
Riya-Raghav <riya.raghav_cs.aiml24@gla.ac.in>
2026-02-20 15:05:16 +0530

    initialize OLED display with defined dimensions and address

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,10 @@
 #include<wire.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3c
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 // TODO 1:
 // Define the DHT data pin (Use digital pin 2)
